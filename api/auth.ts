9f1bff56db1cd0187cb2830380b2bb54391fff97
TafitaNiainaperline <dev@fiharysoft.com>
2026-02-09 12:09:58 +0300

    Refactor: Organize API types into separate files

--- api/auth.ts
+++ api/auth.ts
@@ -1,4 +1,4 @@
-import { LoginRequest, LoginResponse } from './types'
+import { LoginRequest, LoginResponse } from './types/auth'
 
 export async function loginWithJirama(
   credentials: LoginRequest
